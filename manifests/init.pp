76758d66a6fadd247a13e92abc2e85d162204c4f
Justice London <justice@ministrycentered.com>
2014-03-21 15:33:58 -0700

    First commit of scout_realtime puppet script

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+class scout_realtime (
+  $port     = '5555'
+  $log_path = '/var/log/scout_realtime.log'
+  $pid_path = '/var/run/scout_realtime.pid'
+) {
+
+  package { 'scout_realtime':
+    ensure   => present,
+    provider => 'gem',
+  }
+
+  file { '/etc/init/scout_realtime.conf':
+    ensure  => present,
+    content => template('scout_realtime/upstart.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['scout_realtime'],
+  }
+
+  service { 'scout_realtime':
+    ensure  => running,
+    require => File['/etc/init.d/scout_realtime.conf'],
+  }
+
+}
