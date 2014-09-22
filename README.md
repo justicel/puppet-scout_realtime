a944fe8ae000ab9edd4166e2784f283d945a06a9
Justice London <jlondon@syrussystems.com>
2014-09-22 15:15:46 -0700

    Initial commit of module ready for puppet forge.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 puppet-scout_realtime
 =====================
 
-Scout Realtime installation with puppet
+Scout Realtime installation with puppet. More details about scout_realtime: http://scoutapp.github.io/scout_realtime/
+
+Usage
+-----
+
+    class { 'scout_realtime':
+      ensure => present,
+      port   => '5555',
+    }
+
+License
+-------
+
+You are free to fork, modify, burn, break, twist or twine this module.
+If you do re-use the code, please give me credit for it though.
+
+Contact
+-------
+
+Justice London <jlondon@syrussystems.com>
+
+Support
+-------
+
+Please log tickets and issues at our [Projects site](http://github.com/justicel/puppet-scout_realtime)
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,40 +1,103 @@
-#Basic puppet module to install scout-realtime monitor
+# == Class: scout_realtime
+#
+# This module installs the scout realtime ruby gem. This gem allows you to have a local 'top' 
+# service on your system. It generally (by default) runs on port 5555 and depends on ruby
+# greater than 1.8.
+#
+# === Parameters
+# [*ensure*]
+#   Default to present. This sets the package as installed or uninstalled and affects the config as well.
+#   Can be present or absent.
+# [*port*]
+#   The port number to run scout_realtime on. Defaults to port 5555 (default gem port number as well).
+# [*log_path*]
+#   Where to store log-file entries for the scout_realtime service.
+# [*pid_path*]
+#   Location to place the pid file for scout_realtime
+# [*non_system_ruby*]
+#   True/False to use a non-standard gem installation command for the scout_realtime service
+# [*gem_command*]
+#   Command to use for installation and version checking of the scout_realtime gem. Not used
+#   unless non_system_ruby is set to true
+# [*version*]
+#   Version number of scout_realtime gem to install. Version is 1.0.5 as of the time of writing.
+#
+# === Examples
+#
+#  class { 'scout_realtime':
+#    ensure => present,
+#    port   => '5555',
+#  }
+#
+# === Authors
+#
+# Justice London <jlondon@syrussystems.com>
+#
+# === Copyright
+#
+# Copyright 2014 Justice London, unless otherwise noted.
+#
 class scout_realtime (
-  $port        = '5555',
-  $log_path    = '/var/log/scout_realtime.log',
-  $pid_path    = '/var/run/scout_realtime.pid',
-  $use_ruby191 = false,
-  $version     = '1.0.4',
+  $ensure          = present,
+  $port            = '5555',
+  $log_path        = '/var/log/scout_realtime.log',
+  $pid_path        = '/var/run/scout_realtime.pid',
+  $non_system_ruby = false,
+  $gem_command     = 'gem1.9.1',
+  $version         = '1.0.5',
 ) {
 
-  if $use_ruby191 {
+  #Variable verifications
+  validate_re($ensure, '^present$|^absent$')
+  validate_re($port, '^[0-9]+$')
+  validate_absolute_path($log_path)
+  validate_absolute_path($pid_path)
+  validate_bool($non_system_ruby)
+  validate_string($gem_command)
+  validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$')
+
+  #Set present/absent to version or absent
+  $real_version = $ensure ? {
+    present => $version,
+    default => 'absent',
+  }
+  $pres_2bool = $ensure ? {
+    present => true,
+    default => false,
+  }
+
+  if $non_system_ruby {
     exec { 'scout_realtime':
-      command => 'gem1.9.1 install scout_realtime',
-      unless  => "gem1.9.1 list --local | grep scout_realtime | grep ${version}",
+      command => "${gem_command} install scout_realtime",
+      unless  => "${gem_command} list scout_realtime | grep ${version}",
       path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
     }
   }
   else {
-    package { 'scout_realtime':
-      ensure   => $version,
+    ensure_packages('scout_realtime', {
+      ensure => $real_version,
       provider => 'gem',
+    } )
     }
   }
 
   file { '/etc/init/scout_realtime.conf':
-    ensure  => present,
+    ensure  => $ensure,
     content => template('scout_realtime/upstart.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
   }
 
-  $require_scout = $use_ruby191 ? {
+  #Set requirements for service if using a non-system ruby version.
+  $require_scout = $non_system_ruby ? {
     true    => Exec['scout_realtime'],
     default => Package['scout_realtime'],
   }
+
+  $runstate = $ensure
   service { 'scout_realtime':
-    ensure  => running,
+    ensure  => $pres_2bool,
     require => [ $require_scout, File['/etc/init/scout_realtime.conf'], ],
   }
 
